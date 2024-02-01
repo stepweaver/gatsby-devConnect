94325d6fdf8544fcd69b87c5454ad38859a3c0a7
stepweaver <weaverst@mail.gvsu.edu>
2024-01-31 22:34:43 -0500

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <h1>About Me!</h1>
+      <p>
+        I'm baby microdosing chicharrones selfies irony activated charcoal hot
+        chicken, lomo echo park. Chia XOXO mukbang authentic, shaman irony
+        mixtape lumbersexual PBR&B jianbing franzen hell of neutral milk hotel
+        thundercats. Praxis crucifix same mlkshk pop-up try-hard pour-over. Meh
+        meditation kombucha roof party, thundercats XOXO trust fund kogi yr kale
+        chips JOMO. Jean shorts blackbird spyplane heirloom letterpress cupping
+        master cleanse street art, copper mug fingerstache vice la croix.
+        Chambray 8-bit lumbersexual pork belly ennui selvage brunch hashtag
+        tilde irony semiotics microdosing coloring book YOLO green juice. Trust
+        fund tousled scenester marxism jean shorts, fixie glossier bushwick
+        sriracha venmo banjo thundercats photo booth.
+      </p>
+    </>
+  );
+};
+
+export default About;
